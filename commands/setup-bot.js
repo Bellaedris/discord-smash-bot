ec7273cef5335a30379d400af1fdc973ad6cbd2f
Arthur <arthur.pradier81@gmail.com>
2021-01-25 20:01:50 +0100

    Fonctionnalités basiques
    Reprises d'un projet qui trainait dans un coin de mes fichiers

---
+++ commands/setup-bot.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'setup-bot',
+	description: 'Sets up the bot for basic usage',
+    args: false,
+    usage: '',
+    guildOnly: true,
+    cooldown: 2,
+    aliases: ['setup'],
+	execute(message, args) {
+        //TODO
+	},
+};
