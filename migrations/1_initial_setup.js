ae9cb224b7562da5cc3225db5285af76aab275ed
Arthur <arthur.pradier81@gmail.com>
2021-02-01 12:02:23 +0100

    Updated initial creation of tables
    Tables users and combats will be created correctly

--- migrations/1_initial_setup.js
+++ migrations/1_initial_setup.js
@@ -2,31 +2,34 @@
 
 exports.migrate = function (client, done) {
     var db = client.db;
-    var sql = "create table users(" +
+    var sql = ["create table users(" +
             "discordId varchar(100) primary key, " +
             "rating int(5) not null," +
             "previous_rating int(5) not null," +
             "created_at timestamp not null," + 
-            "updated_at timestamp not null);" +
+            "updated_at timestamp not null);",
 
-            "create table match(" +
-            "id int(6) unsigned auto_increment primary key," +
-            "player_one_id varchar(100) NOT NULL" + 
-            "player_one_id varchar(100) NOT NULL" +
-            "winner_id varchar(100)" + 
-            "P1_char varchar(30) NOT NULL" +
-            "P2_char varchar(30) NOT NULL" +
-            "stage varchar(30) NOT NULL"
+            "create table games(" +
+            "id int(6) unsigned auto_increment primary key, " +
+            "player_one_id varchar(100) NOT NULL," + 
+            "player_two_id varchar(100) NOT NULL," +
+            "winner_id varchar(100)," + 
+            "P1_char varchar(30) NOT NULL," +
+            "P2_char varchar(30) NOT NULL," +
+            "stage varchar(30) NOT NULL);"]
 
-    db.query(sql, function(err, result) {
-        if(err) throw err;
+    sql.forEach(query => {
+        db.query(query, function(err, result) {
+            if(err) throw err;
+        });
     });
+    
     done();
 };
 
 exports.rollback = function (client, done) {
     var db = client.db;
-    var sql = "drop table users;" + "drop table match" 
+    var sql = "drop table users;" + "drop table games;" 
     db.query(sql, function(err, result) {
         if(err) throw err;
     });
