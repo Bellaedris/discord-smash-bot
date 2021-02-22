7e248a70e81cb0b5e4a5f1fa7e4457449da704a1
Arthur <arthur.pradier81@gmail.com>
2021-02-22 14:01:50 +0100

    Functionnal match creation

--- commands/match.js
+++ commands/match.js
@@ -1,5 +1,6 @@
 const { prefix } = process.env.BOT_PREFIX;
 const fighters = require('../resources/fighters.json');
+const stages = require('../resources/stages.json');
 
 module.exports = {
     name: 'match',
@@ -16,9 +17,9 @@ module.exports = {
             return message.channel.send("The correct usage is !match @<P1> @<P2>");
         }
 
-        if (args[0] == args[1]) {
+        /*if (args[0] == args[1]) {
             return message.channel.send("You cannot fight yourself.")
-        }
+        }*/
 
         let playerTags = new Array();
         //check both players are subscribed
@@ -62,7 +63,7 @@ module.exports = {
             message.channel.send(args[1] + ", pick your character.")
             const filter = m => m.author.id === playerTags[1] 
             //boucler le collector tant que l'on ne trouve pas de personnage valide?
-            const collector = message.channel.createMessageCollector(filter, {time: 15000});
+            const collector = message.channel.createMessageCollector(filter, {time: 60000});
 
             collector.on('collect', m => {
                 let ret = false;
@@ -82,27 +83,60 @@ module.exports = {
 
             collector.on('end', collected => {
                 picks.forEach(pick => {
-                    pick + " ";
+                    console.log(pick + " ");
                 });
 
                 //pick stage (reaction)
-                let stage = "Battlefield";
+                PlayerToPick = Math.floor(Math.random()); // either 0 or 1
+                selectedStage = ''
+                message.channel.send(args[PlayerToPick] + ", pick the stage.").then(msg => {
 
-                //insert the match data
-                var sql = "insert into games(player_one_id,player_two_id,winner_id,P1_char,P2_char,stage) values('" +
-                playerTags[0] + "','" +
-                playerTags[1] + "'," +
-                "NULL,'" +
-                picks[0] + "','" +
-                picks[1] + "'," +
-                "'" + stage + "');";
+                    stages.forEach(stage => {
+                        msg.react(stage.id);
+                    });
 
-                db.query(sql, function(err, result) {
-                    if(err) throw err;
+                    const filter = (reaction, user) => {
+                        return user.id === playerTags[PlayerToPick];
+                    };
+                    
+                    const collector = msg.createReactionCollector(filter, { time: 60000 });
+                    
+                    collector.on('collect', (reaction, user) => {
+                        let ret = false;
+                        stages.forEach(stage => {
+                            if (stage.emoji == reaction.emoji.name) {
+                                selectedStage = stage.name
+                                ret = true;
+                            }
+                        });
+                        if (ret) {
+                            //if the user input is correct, pick the character
+                            collector.stop();
+                        }
+                    });
+                    
+                    collector.on('end', collected => {
+                        if (!selectedStage.length) {
+                            return message.channel.send("Vous n'avez pas sélectionné de stage. Le match est annulé.");
+                        }
+                        //insert the match data
+                        var sql = "insert into games(player_one_id,player_two_id,winner_id,P1_char,P2_char,stage) values('" +
+                        playerTags[0] + "','" +
+                        playerTags[1] + "'," +
+                        "NULL,'" +
+                        picks[0] + "','" +
+                        picks[1] + "'," +
+                        "'" + selectedStage + "');";
+
+                        db.query(sql, function(err, result) {
+                            if(err) throw err;
+                        });
+
+                        //await for a reaction to know the winner
+                        message.channel.send("ongoing match, react to declare the winner");
+                    });
                 });
-
-                //await for a reaction to know the winner
-                message.channel.send("ongoing match, react to declare the winner");
+            
             })
         });
 
